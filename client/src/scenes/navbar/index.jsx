405d9bf89bebee65e5539f31d60fe1fc022ddab8
sinsar11456 <sinsarsanu111@gmail.com>
2024-01-21 15:29:29 +0530

    styling setup

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FromControl,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import {useDispatch, useSelector} from "rect-redux";
+import { setMode, setLogout } from "state";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "components/FlexBetween";
+
 const Navbar = () => {
-  return <div>navbar</div>;
+  const [isMobileMenuToggled, setIsMobileMenuToggeled] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state)=> state.user);
+  const isMobileSreen = useMediaQuery("(min-width: 1000px)");
+  
+  const theme = useTheme();
+  const neutralLight = theme.palette.neutral.light;
+  const dark = theme.palette.background.default;
+  const background = theme.pallet.primary.light;
+  const alt = theme.pallet.background.alt;
+   
+  const fullName = `${user.firstName} ${user.lastName}`;
+
+ 
+  return 
 };
 
 export default Navbar;
--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -1,5 +1,5 @@
 const ProfilePage = () => {
-  return <div>loginPage</div>;
+  return <div>ProfilePage</div>;
 };
 
 export default ProfilePage;
